7f80bdfbc3e274c2be73d70f594a66623ee6ef60
qiuyue <48547293+qycf@users.noreply.github.com>
2023-09-14 16:03:14 +0800

    Update proxyChecker.js

--- proxyChecker.js
+++ proxyChecker.js
@@ -9,6 +9,7 @@ for (let i = 0; i < nodes.length; i++) {
   }
 }
 let url = `http://192.168.31.109:3001/ipInfo?server=${server}`
+let body = {}
 $httpClient.get(url, function(error, response, data){
 //let jsonData = JSON.parse(data)
 content = '当前：'+ proxy+'\n'+data
@@ -17,8 +18,19 @@ content = '当前：'+ proxy+'\n'+data
     content: content,
     icon: "network"
   }
- $done(body);
 });
+
+$httpClient.get("http://ip-api.com/json", function(error, response, data){
+    let jsonData = JSON.parse(data)
+    let ip = jsonData.query
+    let country = jsonData.country
+    let emoji = getFlagEmoji(jsonData.countryCode)
+    let city = jsonData.city
+    let isp = jsonData.isp
+    body.content += `出口：\nIP: ${ip}\nISP: ${isp}\n位置: ${emoji}${country} - ${city}`
+    $done(body);
+});
+
 
 
 function getFlagEmoji(countryCode) {
